b8312f92927e4f66ad4aa964d678abda7a38789b
Zsolt Tóth <waysofdarkness@gmail.com>
2022-08-20 20:37:46 +0200

    First commit.

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QList>
+#include <QSharedPointer>
+#include <QTextCodec>
+#include <QSet>
+#include <QDir>
+#include <QDirIterator>
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication a(argc, argv);
+	QStringList args;
+	for(int i = 0; i < argc; ++i)
+	{
+		args.push_back(QString::fromUtf8(argv[i]));
+	}
+
+	QTextStream qStdout(stdout);
+	if(argc < 2)
+	{
+		qStdout << "This application requires at least one argument!";
+		return 0;
+	}
+	if(args[1] == "--help")
+	{
+		qStdout << "Usage:\n" << args[0] << " <input directory path> <output directory path>\nOutput directory path is optional.\n";
+		return 0;
+	}
+	QString inputDirPath = args[1];
+	QString outputDirPath;
+	if(argc >= 3) {
+		outputDirPath = args[2];
+	} else outputDirPath = a.applicationDirPath();
+	QTextCodec *codec1 = QTextCodec::codecForName("ISO 8859-15");
+	QDir outDir(outputDirPath);
+	QDirIterator inDir(inputDirPath);
+	QString buffer;
+	while(inDir.hasNext()) {
+		QFileInfo inInfo(inDir.next());
+		QFile inFile(inInfo.absoluteFilePath());
+		QFile outFile(outDir.absoluteFilePath(QStringLiteral("%1.yml").arg(inInfo.baseName())));
+		if(!(inFile.open(QFile::ReadOnly | QFile::Text) && outFile.open(QFile::WriteOnly))) continue;
+		QTextStream inStream(&inFile);
+		QTextStream outStream(&outFile);
+		if(codec1) inStream.setCodec(codec1);
+		outStream.setGenerateByteOrderMark(true);
+		outStream << "l_english:\n";
+		while(inStream.readLineInto(&buffer)) {
+			QStringList splitString = buffer.split(';');
+			if(splitString.size() >= 2) {
+				outStream << ' ' << splitString[0] << ":0 \"" << splitString[1] << "\"\n";
+			}
+		}
+		inFile.close();
+		outStream.flush();
+		outFile.flush();
+		outFile.close();
+	}
+
+
+	return 0;
+}
